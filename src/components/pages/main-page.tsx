2251bf549c829a546e20e21b289008555094e81a
lekting <gorning0@gmail.com>
2021-11-26 15:25:22 +0200

    init

---
+++ src/components/pages/main-page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import Button from '../atoms/buttons/component';
+
+const MainPage: FC = () => (
+  <>
+    <h1 className="ml-5-vw">Hello world !</h1>
+    <Button className="ml-5-vw">Button</Button>
+  </>
+);
+
+export default MainPage;
